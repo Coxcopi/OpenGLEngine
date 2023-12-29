9e89ee7e0b5aa575d2835e492fee52bc505c41ff
Coxcopi <85553012+Coxcopi@users.noreply.github.com>
2023-12-29 15:57:47 +0100

    additional comments in Mesh

--- src/main/java/de/coxcopi/mesh/Mesh.java
+++ src/main/java/de/coxcopi/mesh/Mesh.java
@@ -7,15 +7,32 @@ import org.lwjgl.opengl.GL30;
 
 public class Mesh {
 
+    /**
+     * The mesh's global (world) transform.
+     */
     public Matrix4 transform;
+    /**
+     * The mesh's material.
+     */
     public Material material;
     /**
      * If false, the mesh is not rendered.
      */
     public boolean visible;
+    /**
+     * The vertex attribute object.
+     */
     private final int vao;
+    /**
+     * Amount of elements (indices).
+     */
     private final int elements;
 
+    /**
+     * Constructs a new mesh with the given parameters.
+     * @param vao The vertex attribute object.
+     * @param elements The amount of elements (indices).
+     */
     public Mesh(int vao, int elements) {
         this.vao = vao;
         this.elements = elements;
@@ -24,17 +41,25 @@ public class Mesh {
         this.visible = true;
     }
 
+    /**
+     * @return The amount of indices.
+     */
     public int getElementCount() {
         return elements;
     }
 
+    /**
+     * Renders the mesh by binding the material and shaders,
+     * setting shader uniforms and finally binding
+     * the vao.
+     */
     public void render() {
         if (!visible) {
             return;
         }
         material.bind();
         material.shader.setDefaultMatrixUniforms(transform, Renderer.camera.viewMatrix, Renderer.camera.projectionMatrix);
-        material.shader.setLightingUniforms(Renderer.environment.getBackgroundColor(), Renderer.camera.getPosition());
+        material.shader.setLightingUniforms(Renderer.environment.getBackgroundColor(), Renderer.camera.transform.getOrigin());
         GL30.glBindVertexArray(vao);
     }
 }
